382d4aca6ad2a753cb62c5979e12e96c41ee89b8
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2023-10-20 04:10:24 +0200

    Refactoring and creating the “patients.html” view with Thymeleaf

--- src/main/java/com/dgmf/web/controller/PatientController.java
+++ src/main/java/com/dgmf/web/controller/PatientController.java
@@ -6,28 +6,22 @@ import com.dgmf.web.dto.PatientDtoResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/api/v1/patients")
+// @RestController
+// @RequestMapping("/api/v1/patients")
+@Controller
 @RequiredArgsConstructor
 public class PatientController {
     private final PatientService patientService;
 
-    @PostMapping("/create")
-    public ResponseEntity<PatientDtoResponse> createPatient(
-            @RequestBody PatientDtoRequest patientDtoRequest
-            ) {
-        return new ResponseEntity<>(
-                patientService.createPatient(patientDtoRequest),
-                HttpStatus.CREATED
-        );
-    }
-
-    @GetMapping
-    public ResponseEntity<List<PatientDtoResponse>> getAllPatients() {
-        return ResponseEntity.ok(patientService.getAllPatients());
+    // http://localhost:8080/index
+    @GetMapping("/index")
+    public String patients() {
+        return "patients";
     }
 }
